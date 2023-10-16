4f819203d83d7bf487304e5c26d6e0257aa5ff03
rot_will <2127361651@qq.com>
2023-10-16 17:14:26 +0800

    Site updated: 2023-10-16 17:14:26

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,38 @@
 (function(window, document) {
-function change_style(){
-    var parentElement = document.querySelector("[class='markdown-body']");
+function change_style(isenc){
+    if (isenc==true)
+        var parentElement=document.getElementById("hexo-blog-encrypt").childNodes[0];
+    else
+        var parentElement = document.querySelector("[class='markdown-body']");
+
     var pTags = parentElement.querySelectorAll(":scope>p");
     for(var i=0;i<pTags.length;i++){
 
-        console.log(pTags[i]);
         var child_es=pTags[i].querySelectorAll("img");
-        console.log(child_es);
         if (child_es.length==0){
             pTags[i].classList.add('salient');
         }
     }
 }
-change_style();
+
+function loadImage(){
+    for (const each of document.querySelectorAll('img[lazyload]')) {
+        Fluid.utils.waitElementVisible(each, function() {
+          each.removeAttribute('srcset');
+          each.removeAttribute('lazyload');
+        }, CONFIG.lazyload.offset_factor);
+      }
+}
+
+const hdeElement=document.getElementById('hexo-blog-encrypt');
+if (hdeElement==null)
+    change_style();
+else{
+    window.addEventListener('hexo-blog-decrypt',(
+        function(){
+            change_style(true);
+            loadImage();
+            }));
+}
 })(window,document)
+
